8f1e9f4570329e7bde1583695046d4e263de3f4f
Mujtaba Omar <mujtabaomar123@gmail.com>
2024-11-25 23:00:35 +0500

    initialized app

---
+++ README.md
@@ -0,0 +1,3 @@
+# Degree Verification Mobile App Based on Blockchain
+
+Replacing bureaucracy one step at a time.
---
+++ android/app/src/main/kotlin/com/example/degree_verifier/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.degree_verifier
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
